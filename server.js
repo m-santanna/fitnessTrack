ed906948f24c7206049904bb8cd366f788a9e291
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2023-09-22 20:26:43 +0100

    error handler added

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`app running at ${PORT}`);
 });
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
